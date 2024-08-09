affd1082d953204f5035d613edde9bc8b3efdbb9
Prasanna Bhat <prasannabhat018@gmail.com>
2024-08-09 23:28:58 +0530

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Prasanna Bhat on 09/08/24.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
